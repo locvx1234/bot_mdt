09b95ca147e3c8371382f73d7f8a3f79e61e87f2
MinhKMA <01666098998minh@gmail.com>
2018-02-26 23:12:39 +0700

    create vm in openstack by telegram bot

--- telebot/plugins/draft/create_vm.py
+++ telebot/plugins/draft/create_vm.py
@@ -7,88 +7,94 @@ from neutronclient.v2_0 import client
 import glanceclient.client as glanceclient
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
+TELEGRAM_HTTP_API_TOKEN = '529971138:AAHaRo7KYsjfCwUhe9x3r7ilFftEDUwNyKM'
 
-def keyboard_item(list_items):
-    keyboard_items = []
-    id_items = []
-    for item in list_items:
-        data_items = item.split(' : ')
-        name_item = data_items[0]
-        id_item = data_items[1]
-        keyboard_items.append([InlineKeyboardButton(name_item,
-                                                 callback_data=id_item)])
-        id_items.append(id_item)
-    keyboard_items.append([InlineKeyboardButton('back', callback_data='back')])
-    return keyboard_items, id_items
-
-def dm(bot, update):
-
-    keyboard = [[InlineKeyboardButton("networks", callback_data='network'),
-                 InlineKeyboardButton("images",
-                                      callback_data='image')]]
-
+FIRST, SECOND, THIRD, FOURTH = range(4)
+data = {}
+def start(bot, update):
+    keyboard = [
+        [InlineKeyboardButton(u"Network", callback_data=str(FIRST))]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        u"Start go to hera, Press network",
+        reply_markup=reply_markup
+    )
+    return FIRST
 
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-def button(bot, update):
-    keyboard = [[InlineKeyboardButton("networks", callback_data='network'),
-                 InlineKeyboardButton("images",
-                                      callback_data='image')]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-
-    list_networks, list_images = create_vm()
+def first(bot, update):
+    query = update.callback_query
+    list_networks, list_flavors, list_images = create_vm()
     keyboard_network, id_networks = keyboard_item(list_networks)
-    keyboard_image, id_images = keyboard_item(list_images)
-    reply_markup_network = InlineKeyboardMarkup(keyboard_network)
-    reply_markup_image = InlineKeyboardMarkup(keyboard_image)
-    # bot.edit_message_text(text="Selected option - dkm: {}".format(query.data),
-    #                        chat_id=query.message.chat_id,
-    #                        message_id=query.message.message_id)
-    if update.callback_query.data == 'network':
-        # list_networks, list_images = create_vm()
-        # keyboard_network, id_items = keyboard_item(list_networks)
-        # reply_markup_network = InlineKeyboardMarkup(keyboard_network)
-        bot.edit_message_text(text="Selected option: {}".format(update.callback_query.data),
-                          chat_id=update.callback_query.message.chat_id,
-                          message_id=update.callback_query.message.message_id,
-                              reply_markup=reply_markup_network)
-        # print(list_networks)
-        # print(id_networks)
+    print(query.data)
+    bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text=u"Select network"
+    )
+
+    reply_markup = InlineKeyboardMarkup(keyboard_network)
+
+    bot.edit_message_reply_markup(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        reply_markup=reply_markup
+    )
+    #print(query.data)
+    return SECOND
+
+def second(bot, update):
+    query = update.callback_query
+    data["network"] = query.data
+    list_networks, list_flavors, list_images = create_vm()
+    keyboard_images, id_images = keyboard_item(list_images)
+    bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text=u"select image"
+    )
+    reply_markup = InlineKeyboardMarkup(keyboard_images)
+    bot.edit_message_reply_markup(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        reply_markup=reply_markup)
+    return THIRD
 
 
-    if update.callback_query.data == 'image':
-        # list_networks, list_images = create_vm()
-        # keyboard_image = keyboard_item(list_images)
-        # reply_markup_image = InlineKeyboardMarkup(keyboard_image)
-        bot.edit_message_text(text="Selected option: {}".format(update.callback_query.data),
-                              chat_id=update.callback_query.message.chat_id,
-                              message_id=update.callback_query.message.message_id,
-                              reply_markup=reply_markup_image)
+def third(bot, update):
+    query = update.callback_query
+    data["image"] = query.data
+    list_networks, list_flavors, list_images = create_vm()
+    keyboard_flavors, id_images = keyboard_item(list_flavors)
+    bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text=u"select flavor"
+    )
+    reply_markup = InlineKeyboardMarkup(keyboard_flavors)
+    bot.edit_message_reply_markup(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        reply_markup=reply_markup)
+    return FOURTH
 
-    if update.callback_query.data == 'back':
-        bot.edit_message_text(text="Selected option: ",
-                              chat_id=update.callback_query.message.chat_id,
-                              message_id=update.callback_query.message.message_id,
-                              reply_markup=reply_markup)
-
-    if update.callback_query.data in id_networks:
-        network = update.callback_query.data
-        print("id network is : {}".format(network))
-
-    if update.callback_query.data in id_images:
-        image = update.callback_query.data
-        print("id image is : {}".format(image))
-
-
-
+def fourth(bot, update):
+    query = update.callback_query
+    data['flavor'] = query.data
+    print(data)
+    bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text=str(data))
+    sess = authenticate()
+    nova = novaclient.Client("2.1", session=sess)
+    nova.servers.create(name = "minhkma",
+                        image= data["image"],
+                        flavor= data['flavor'],
+                        nics= [{'net-id':data['network']}])
+    return
 
 def authenticate(auth_url= None, username= None ,password= None ,
                  project_name= None ):
@@ -112,12 +118,20 @@ def create_vm():
     sess = authenticate()
     neutron = client.Client(session=sess)
     networks = neutron.list_networks()
-    list_network = []
+    list_networks = []
     for count_network in range(len(networks["networks"])):
         Name = networks["networks"][count_network]["name"]
         ID = networks["networks"][count_network]["id"]
-        button_network = '{0} : {1}'.format(Name, ID)
-        list_network.append(button_network)
+        button_networks = '{0} : {1}'.format(Name, ID)
+        list_networks.append(button_networks)
+    list_flavors = []
+    nova = novaclient.Client("2.1", session=sess)
+    flavors = nova.flavors.list()
+    for flavor in flavors:
+        id_flavor = flavor.id
+        name_flavor = flavor.name
+        button_flavors = '{0} : {1}'.format(name_flavor,id_flavor)
+        list_flavors.append(button_flavors)
     list_images = []
     glance = glanceclient.Client('2', session=sess)
     for image in glance.images.list():
@@ -125,33 +139,37 @@ def create_vm():
         id = image["id"]
         button_images = '{0} : {1}'.format(name,id)
         list_images.append(button_images)
-    return list_network, list_images
+    return list_networks, list_flavors, list_images
 
-def help(bot, update):
-    update.message.reply_text("Use /dm to test this bot.")
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
-
-
-def main():
-    updater = Updater("529971138:AAHaRo7KYsjfCwUhe9x3r7ilFftEDUwNyKM")
-
-    updater.dispatcher.add_handler(CommandHandler('dm', dm))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+def keyboard_item(list_items):
+    keyboard_items = []
+    id_items = []
+    for item in list_items:
+        data_items = item.split(' : ')
+        name_item = data_items[0]
+        id_item = data_items[1]
+        keyboard_items.append([InlineKeyboardButton(name_item,
+                                                 callback_data=id_item)])
+        id_items.append(id_item)
+    #keyboard_items.append([InlineKeyboardButton('back', callback_data='back')])
+    return keyboard_items, id_items
 
 
+updater = Updater(TELEGRAM_HTTP_API_TOKEN)
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        FIRST: [CallbackQueryHandler(first)],
+        SECOND: [CallbackQueryHandler(second)],
+        THIRD: [CallbackQueryHandler(third)],
+        FOURTH: [CallbackQueryHandler(fourth)]
+    },
+    fallbacks=[CommandHandler('start', start)]
+)
+
+updater.dispatcher.add_handler(conv_handler)
+
+updater.start_polling()
+
+updater.idle()
